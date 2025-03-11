4276cdb6fbaead26f0b3e2698859f74af826d9b7
Marijs Biebuyck <marijs.biebuyck@telenet.be>
2025-03-11 14:53:05 +0100

    PostCSS

--- eslint.config.js
+++ eslint.config.js
@@ -7,3 +7,17 @@ export default [
   {languageOptions: { globals: globals.browser }},
   pluginJs.configs.recommended,
 ];
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: 'eslint:recommended',
+	overrides: [],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+	},
+	rules: {},
+};
---
+++ postcss.config.cjs
@@ -0,0 +1,13 @@
+module.exports = {
+    plugins: [
+      require('autoprefixer'),
+      [
+        'postcss-preset-env',
+        {
+          // Options
+          features: {},
+        },
+      ],
+    ]
+  }
+  
